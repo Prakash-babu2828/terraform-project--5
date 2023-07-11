5f6edec4d5c6326f11924f9dd4b7733d225270cd
Prakash-babu2828 <132740719+Prakash-babu2828@users.noreply.github.com>
2023-07-11 02:40:37 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -90,7 +90,7 @@ resource "aws_security_group" "five" {
 }
 
 resource "aws_s3_bucket" "six" {
-  bucket = "rahamshaikterra77889900prodenv"
+  bucket = "prakash28281997"
 }
 
 resource "aws_iam_user" "seven" {
